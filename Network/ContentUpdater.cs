962c6d4597fbed2b2a03ebcc0721615eeb8e6697
EnergyCube <dev@energycube.fr>
2020-12-20 01:20:16 +0100

    Improvement of updates and Campaigns support

---
+++ Network/ContentUpdater.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace EELauncher
+{
+    class ContentUpdater
+    {
+        private GameHelper gameHelper;
+
+        private Label label;
+        private ProgressBar progressBar;
+        private SynchronizationContext synchronizationContextUI;
+
+        public ContentUpdater(GameHelper gameHelper, Label label, ProgressBar progressBar, SynchronizationContext synchronizationContextUI)
+        {
+            this.gameHelper = gameHelper;
+            this.label = label;
+            this.progressBar = progressBar;
+            this.synchronizationContextUI = synchronizationContextUI;
+        }
+
+        // Download lang.dll
+        public void UpdateLang(string lang)
+        {
+            // Copy from repo to game folder if needed
+            bool langRemote = CheckSumHelper.CheckFromFileAndURL(CheckSumHelper.CheckSumAlgo.CRC,
+                gameHelper.GetInstallFolder().FullName + @"\Language.dll",
+                "http://storage.empireearth.eu/EE/Languages/" + lang + "/Language.dll");
+
+            if (!langRemote)
+            {
+                new FileDownloadHelper(label, progressBar, synchronizationContextUI).DownloadOneFile("http://storage.empireearth.eu/EE/Languages/" + lang + "/Language.dll",
+                    "./Languages/" + lang + "/Language.dll");
+            }
+
+            string langProgram = CheckSumHelper.GetFromFile(CheckSumHelper.CheckSumAlgo.CRC, gameHelper.GetInstallFolder().FullName + @"\Language.dll");
+            string langLocal = CheckSumHelper.GetFromFile(CheckSumHelper.CheckSumAlgo.CRC, "./Languages/" + lang + "/Language.dll");
+
+            if (!langLocal.Equals(langProgram))
+            {
+                File.Copy("./Languages/" + lang + "/Language.dll", gameHelper.GetInstallFolder().FullName + @"/Language.dll", true);
+            }
+        }
+
+        // Only copy from local repo to game
+        public void UpdateLangOffline(string lang)
+        {
+            // Copy from repo to game folder if needed
+            string langProgram = CheckSumHelper.GetFromFile(CheckSumHelper.CheckSumAlgo.CRC, gameHelper.GetInstallFolder().FullName + @"\Language.dll");
+            string langLocal = CheckSumHelper.GetFromFile(CheckSumHelper.CheckSumAlgo.CRC, "./Languages/" + lang + "/Language.dll");
+            if (!langLocal.Equals(langProgram))
+            {
+                File.Copy("./Languages/" + lang + "/Language.dll", gameHelper.GetInstallFolder().FullName + @"/Language.dll", true);
+            }
+        }
+
+        // Download EE.bik
+        public void UpdateIntro(bool skipIntro, string lang)
+        {
+            DirectoryInfo moviesDirectoryInfo = new DirectoryInfo(gameHelper.GetInstallFolder().FullName + @"\Data\Movies");
+            DirectoryInfo moviesDisabledDirectoryInfo = new DirectoryInfo(gameHelper.GetInstallFolder().FullName + @"\Data\_Movies");
+            try
+            {
+                if (skipIntro)
+                {
+                    if (moviesDirectoryInfo.Exists)
+                        moviesDirectoryInfo.MoveTo(moviesDisabledDirectoryInfo.FullName);
+                }
+                else
+                {
+                    if (moviesDisabledDirectoryInfo.Exists)
+                        moviesDisabledDirectoryInfo.MoveTo(moviesDirectoryInfo.FullName);
+
+                    new FileDownloadHelper(label, progressBar, synchronizationContextUI)
+                        .DownloadOneFile("http://storage.empireearth.eu/EE/Languages/" + lang + "/Data/Movies/Empire%20Earth.bik",
+                        gameHelper.GetInstallFolder().FullName + @"Data\Movies\Empire Earth.bik");
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unnable to rename Movies folder !\nIs the game already running ?\n Error : " + ex.Message);
+            }
+        }
+
+        public void UpdateData(string lang)
+        {
+            Dictionary<string, string> dataDictionary = new Dictionary<string, string>();
+            dataDictionary.Add("http://storage.empireearth.eu/EE/Languages/" + lang + "/Data/data.ssa",
+                    gameHelper.GetInstallFolder().FullName + "/Data/data.ssa");
+
+            dataDictionary.Add("https://storage.empireearth.eu/EE/Languages/" + lang + "/Data/Campaigns/EELearningCampaign.ssa",
+                    gameHelper.GetInstallFolder().FullName + "/Data/Campaigns/EELearningCampaign.ssa");
+            dataDictionary.Add("http://storage.empireearth.eu/EE/Languages/" + lang + "/Data/Campaigns/EETheBritish.ssa",
+                    gameHelper.GetInstallFolder().FullName + "/Data/Campaigns/EETheBritish.ssa");
+            dataDictionary.Add("http://storage.empireearth.eu/EE/Languages/" + lang + "/Data/Campaigns/EETheFuture.ssa",
+                    gameHelper.GetInstallFolder().FullName + "/Data/Campaigns/EETheFuture.ssa");
+            dataDictionary.Add("http://storage.empireearth.eu/EE/Languages/" + lang + "/Data/Campaigns/EETheGermans.ssa",
+                    gameHelper.GetInstallFolder().FullName + "/Data/Campaigns/EETheGermans.ssa");
+            dataDictionary.Add("http://storage.empireearth.eu/EE/Languages/" + lang + "/Data/Campaigns/EETheGreeks.ssa",
+                    gameHelper.GetInstallFolder().FullName + "/Data/Campaigns/EETheGreeks.ssa");
+
+            new FileDownloadHelper(label, progressBar, synchronizationContextUI)
+                .DownloadFromList(dataDictionary);
+        }
+
+        public void UpdateDrex(bool enabled, string config)
+        {
+            FileInfo fileInfoDrex = new FileInfo(gameHelper.GetInstallFolder().FullName + "/dreXmod.dll");
+            FileInfo fileInfoDrexConfig = new FileInfo(gameHelper.GetInstallFolder().FullName + "/dreXmod.config");
+            FileInfo fileInfoDrexDisabled = new FileInfo(gameHelper.GetInstallFolder().FullName + "/dreXmod._dll");
+
+            if (enabled)
+            {
+                if (fileInfoDrexDisabled.Exists)
+                    fileInfoDrexDisabled.MoveTo(fileInfoDrex.FullName);
+
+                new FileDownloadHelper(label, progressBar, synchronizationContextUI)
+                    .DownloadOneFile("http://storage.empireearth.eu/Launcher/dreXmod/dreXmod.dll", fileInfoDrex.FullName);
+                
+                new FileDownloadHelper(label, progressBar, synchronizationContextUI)
+                    .DownloadOneFile("http://storage.empireearth.eu/Launcher/dreXmod/preset/" + config + ".config",
+                    fileInfoDrexConfig.FullName);
+            }
+            else
+            {
+                if (fileInfoDrex.Exists)
+                    fileInfoDrex.MoveTo(fileInfoDrexDisabled.FullName);
+                if (fileInfoDrexConfig.Exists)
+                    fileInfoDrexConfig.Delete();
+            }
+        }
+
+    }
+}
---
+++ Network/FileDownloadHelper.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace EELauncher
+{
+    class FileDownloadHelper
+    {
+        private Stopwatch sw = new Stopwatch();
+
+        object syncObject = new object();
+
+        private Dictionary<string, string> localDownloadList;
+        private List<Uri> downloadLinkList = null;
+        private int downloadListIndex = 0;
+
+        private string currentFileName = null;
+
+        private Label label;
+        private ProgressBar progressBar;
+        private SynchronizationContext synchronizationContextUI;
+
+        // This Class is to Download File Asy with UI
+        public FileDownloadHelper(Label label, ProgressBar progressBar, SynchronizationContext synchronizationContextUI)
+        {
+            this.label = label;
+            this.progressBar = progressBar;
+            this.synchronizationContextUI = synchronizationContextUI;
+        }
+
+        public void DownloadOneFile(string url, string path)
+        {
+            url.Replace("https:", "http:");
+
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+                synchronizationContextUI.Post(new SendOrPostCallback((o) =>
+                {
+                    progressBar.Value = 0;
+                    if(progressBar.Style != ProgressBarStyle.Marquee)
+                        progressBar.Style = ProgressBarStyle.Marquee;
+                    progressBar.Visible = true;
+                }), null);
+
+                synchronizationContextUI.Post(new SendOrPostCallback((o) =>
+                {
+                    label.Text = "Checking File Integrity...";
+                    label.Visible = true;
+                }), null);
+
+                sw.Start();
+
+                bool currentCheckSum = CheckSumHelper.CheckFromFileAndURL(CheckSumHelper.CheckSumAlgo.CRC, path, url);
+
+                if (!currentCheckSum)
+                {
+                    if (File.Exists(path))
+                        File.Delete(path);
+
+                    if (!new FileInfo(path).Directory.Exists)
+                        new FileInfo(path).Directory.Create();
+
+                    synchronizationContextUI.Post(new SendOrPostCallback((o) =>
+                    {
+                        progressBar.Style = ProgressBarStyle.Blocks;
+                    }), null);
+
+
+                    lock (syncObject)
+                    {
+                        currentFileName = Path.GetFileName(path);
+                        Console.WriteLine("Downloading " + currentFileName + " [" + url + "]");
+                        webClient.DownloadFileAsync(new Uri(url), path);
+                        //This would block the thread until download completes
+                        Monitor.Wait(syncObject);
+                    }
+
+                }
+                else
+                {
+                    Completed(null, null);
+                }
+            }
+        }
+
+        public void DownloadFromList(Dictionary<string, string> downloadList)
+        {
+            localDownloadList = new Dictionary<string, string>(downloadList.Count);
+            downloadLinkList = new List<Uri>();
+            downloadListIndex = 0;
+
+            foreach (string keyCollection in downloadList.Keys)
+                downloadLinkList.Add(new Uri(keyCollection.Replace("https:", "http:")));
+
+            foreach (string keyCollection in downloadList.Keys)
+                localDownloadList.Add(keyCollection.Replace("https:", "http:"), downloadList[keyCollection]);
+
+                DownloadOneFile(downloadLinkList[downloadListIndex].AbsoluteUri,
+                     localDownloadList[downloadLinkList[downloadListIndex].AbsoluteUri]);
+
+        }
+
+        private DateTime wanted_ui_time = DateTime.Now;
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            if (DateTime.Now >= wanted_ui_time)
+            {
+                synchronizationContextUI.Post(new SendOrPostCallback((o) =>
+                {
+                    label.Text = string.Format(currentFileName + "  |  {0} MB/s  |  {1} %  ({2}/{3} MB)",
+                   (e.BytesReceived / 1024d / 1024d / sw.Elapsed.TotalSeconds).ToString("0.00"),
+                   e.ProgressPercentage.ToString(),
+                   (e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+                   (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"));
+                }), null);
+
+                synchronizationContextUI.Post(new SendOrPostCallback((o) =>
+                {
+                    progressBar.Value = e.ProgressPercentage;
+                }), null);
+
+                wanted_ui_time = DateTime.Now.AddMilliseconds(25);
+            }
+
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            // Progress Bar
+            synchronizationContextUI.Post(new SendOrPostCallback((o) =>
+            {
+                progressBar.Visible = false;
+                progressBar.Value = 0;
+            }), null);
+
+            // Label
+            synchronizationContextUI.Post(new SendOrPostCallback((o) =>
+            {
+                label.Visible = false;
+                label.Text = "";
+            }), null);
+
+            sw.Reset();
+
+            // Check more download to do
+            if (downloadLinkList != null)
+            {
+                if (downloadLinkList.Count != downloadListIndex + 1)
+                {
+                    downloadListIndex++;
+                    DownloadOneFile(downloadLinkList[downloadListIndex].AbsoluteUri,
+                        localDownloadList[downloadLinkList[downloadListIndex].AbsoluteUri]);
+                }
+                else
+                {
+                    lock (syncObject)
+                    {
+                        Monitor.PulseAll(syncObject);
+                    }
+                }
+            }
+            else
+            {
+                lock (syncObject)
+                {
+                    Monitor.PulseAll(syncObject);
+                }
+            }
+        }
+    }
+}
